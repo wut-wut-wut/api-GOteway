a1cd5668c6de485a8de257ece10cffd39ed7016c
wut-wut-wut <wut-wut-wut@users.noreply.github.com>
2021-06-09 18:09:16 +0200

    reformat code

--- main.go
+++ main.go
@@ -1,73 +1,67 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "regexp"
+	"fmt"
+	"net/http"
+	"regexp"
 )
 
 type FilterContext struct {
-    Filter *FilterConfig
-    R *http.Request
-    TargetURL string
-    RequestURI string
+	Filter     *FilterConfig
+	R          *http.Request
+	TargetURL  string
+	RequestURI string
 }
 
 type RouteFilter interface {
-    Process(f *FilterContext) bool
+	Process(f *FilterContext) bool
 }
 
 func main() {
-    c := GetConfig()
-    f := InitFilters(c.Routes)
-    
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        route := getMatchingRoute(r.URL.Path, c.Routes)
-          
-        if route == nil {
-            http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-        } else {
-             //fmt.Fprintf(w, "route: " + route.Name)
-             
-             fc := FilterContext{}
-             fc.Filter = &route.Filters[0]
-             fc.RequestURI = r.URL.RequestURI()
-             fc.TargetURL = route.Url
-            
-            for _, filter := range f {
-                 filter.Process(&fc)
-             }
-             
-             DoRequest(&fc, w, r)
-    
-        }
-    })
+	c := GetConfig()
+	f := InitFilters(c.Routes)
 
-    fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		route := getMatchingRoute(r.URL.Path, c.Routes)
 
-    http.ListenAndServe(":8000", nil)
+		if route == nil {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		} else {
+			fc := FilterContext{}
+			fc.Filter = &route.Filters[0]
+			fc.RequestURI = r.URL.RequestURI()
+			fc.TargetURL = route.Url
+
+			for _, filter := range f {
+				filter.Process(&fc)
+			}
+
+			DoRequest(&fc, w, r)
+		}
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
 
 func getMatchingRoute(path string, routes []RouteConfig) *RouteConfig {
-    var matchingRoute *RouteConfig = nil
-    for i, route := range routes {
-        sampleRegex := regexp.MustCompile("^"+route.Path)
-        match := sampleRegex.MatchString(path)
-        if match {
-            fmt.Printf("%s and %s : %t \n", path, route.Path, match)
-            matchingRoute = &routes[i]
-        }
-    }
-    
-    return matchingRoute
+	var matchingRoute *RouteConfig = nil
+	for i, route := range routes {
+		sampleRegex := regexp.MustCompile("^" + route.Path)
+		match := sampleRegex.MatchString(path)
+		if match {
+			fmt.Printf("%s and %s : %t \n", path, route.Path, match)
+			matchingRoute = &routes[i]
+		}
+	}
+
+	return matchingRoute
 }
 
 func InitFilters(rc []RouteConfig) []RouteFilter {
-    rfs := make([]RouteFilter, 1)
-    
-    var rf RouteFilter = StripPrefixFilter{}
-    rfs[0] = rf
-    
-    return rfs
+	rfs := make([]RouteFilter, 1)
+
+	var rf RouteFilter = StripPrefixFilter{}
+	rfs[0] = rf
+
+	return rfs
 }
--- stripprefixfilter.go
+++ stripprefixfilter.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "strconv"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 type StripPrefixFilter struct {
-
 }
 
-func (spf StripPrefixFilter)  Process(f *FilterContext) bool {
-    fmt.Println("Process StripPrefixFilter")
-    depth, exists := f.Filter.Properties["depth"]
-     d, _ := strconv.Atoi(depth)
-    if exists {
-        fmt.Println("Depth : " + depth)
-        fmt.Println("URI: " + f.RequestURI)
-        for cpt := 0; cpt <= d; cpt++ {
-            i := strings.Index(f.RequestURI, `/`)
-            if i > -1 {
-                fmt.Println("Index: ", i)
-                f.RequestURI = f.RequestURI[i+1:]
-            } else {
-                fmt.Println("Index not found")
-            }
-        }
-        
-        if f.RequestURI[0] != '/' {
-            f.RequestURI = `/` + f.RequestURI
-        }
-        
-        fmt.Println(f.RequestURI)
-    }
-    return true
+func (spf StripPrefixFilter) Process(f *FilterContext) bool {
+	fmt.Println("Process StripPrefixFilter")
+	depth, exists := f.Filter.Properties["depth"]
+	d, _ := strconv.Atoi(depth)
+	if exists {
+		fmt.Println("Depth : " + depth)
+		fmt.Println("URI: " + f.RequestURI)
+		for cpt := 0; cpt <= d; cpt++ {
+			i := strings.Index(f.RequestURI, `/`)
+			if i > -1 {
+				fmt.Println("Index: ", i)
+				f.RequestURI = f.RequestURI[i+1:]
+			} else {
+				fmt.Println("Index not found")
+			}
+		}
+
+		if len(f.RequestURI) > 0 {
+			if f.RequestURI[0] != '/' {
+				f.RequestURI = `/` + f.RequestURI
+			}
+		} else {
+			f.RequestURI = `/`
+		}
+	}
+	return true
 }
