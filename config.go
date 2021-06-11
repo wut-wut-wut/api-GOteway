70252ae520355a67e464e281b20ee0a428e0ecc2
wut-wut-wut <wut-wut-wut@users.noreply.github.com>
2021-06-11 19:22:11 +0200

    merge

--- config.go
+++ config.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"flag"
 	"io/ioutil"
 	"log"
@@ -52,48 +51,3 @@ func GetConfig() Config {
 
 	return c
 }
-=======
-    "fmt"
-    "gopkg.in/yaml.v2"
-)
-
-type Config struct {
-    Routes []RouteConfig
-}
-
-type RouteConfig struct {
-    Name string
-    Path string
-    Url string
-    Filters []FilterConfig
-}
-
-type FilterConfig struct {
-    Name string
-    Properties map[string]string
-}
-
-func GetConfig() Config{
-    c := Config{}
-    var data = `
-    routes:
-        - name: cars
-          path: /cars/*
-          url: https://localhost:8080
-          filters:
-            - name: stripPrefix
-              properties:
-                depth: 1
-        - name: houses
-          path: /houses/*
-          url: http://localhost:8001
-    `
-    err := yaml.Unmarshal([]byte(data), &c)
-    if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Printf("--- c:\n%v\n\n", c)
-
-    return c
-}
->>>>>>> 68f7fd6346f1a0fa3e020d287e27fc2341237346
--- stripprefixfilter.go
+++ stripprefixfilter.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"net/http"
 	"strconv"
 	"strings"
@@ -31,39 +30,3 @@ func (spf StripPrefixFilter) Process(f *FilterContext, w http.ResponseWriter, r 
 	}
 	return nil
 }
-=======
-    "fmt"
-    "strings"
-    "strconv"
-)
-
-type StripPrefixFilter struct {
-
-}
-
-func (spf StripPrefixFilter)  Process(f *FilterContext) bool {
-    fmt.Println("Process StripPrefixFilter")
-    depth, exists := f.Filter.Properties["depth"]
-     d, _ := strconv.Atoi(depth)
-    if exists {
-        fmt.Println("Depth : " + depth)
-        fmt.Println("URI: " + f.RequestURI)
-        for cpt := 0; cpt <= d; cpt++ {
-            i := strings.Index(f.RequestURI, `/`)
-            if i > -1 {
-                fmt.Println("Index: ", i)
-                f.RequestURI = f.RequestURI[i+1:]
-            } else {
-                fmt.Println("Index not found")
-            }
-        }
-        
-        if f.RequestURI[0] != '/' {
-            f.RequestURI = `/` + f.RequestURI
-        }
-        
-        fmt.Println(f.RequestURI)
-    }
-    return true
-}
->>>>>>> 68f7fd6346f1a0fa3e020d287e27fc2341237346
